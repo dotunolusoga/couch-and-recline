7d98780863d5a44a614c36d089e4f7639d0eb9a4
Dotun Olusoga <adedotun.olusoga@gmail.com>
2015-07-29 11:37:22 -0500

    WIP: Contact functionality

--- scripts/main.controller.js
+++ scripts/main.controller.js
@@ -4,7 +4,7 @@ angular
 	.controller('ShowCaseController', ShowCaseController);
 
 
-function ShowCaseController ($http) {
+function ShowCaseController ($http, $rootScope) {
 	var vm = this,
 	BASE_URL = new Firebase('https://2com.firebaseio.com/');
 
@@ -12,5 +12,23 @@ function ShowCaseController ($http) {
 		.get(BASE_URL + '/furniture.json')
 		.success(function(data) {
 			vm.furniture = data;
-		})
+		});
+
+	vm.getContact = function () {
+		vm.newContact.contactName;
+		vm.newContact.email;
+
+		$http
+			.post(BASE_URL + '/contacts.json',
+			 {name: vm.newContact.contactName,
+				email: vm.newContact.email})
+			.success(function() {
+					$location.path('/')
+					clearForm();
+				})
+		}
+
+		function clearForm () {
+			contactForm.empty();
+		}
 };
